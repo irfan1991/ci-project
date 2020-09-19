bf614a9f9ac3f2fdc3c7dd0ea194796362cba882
irfan1991 <irfanprasetyo91@gmail.com>
2020-09-19 09:59:02 +0700

    auth

--- app/Controllers/ApiProduct.php
+++ app/Controllers/ApiProduct.php
@@ -1,15 +1,73 @@
 <?php 
 namespace App\Controllers;
 
+// Panggil JWT
+use \Firebase\JWT\JWT;
+// panggil class Auht
+use App\Controllers\Auth;
+// panggil restful api codeigniter 4
+use CodeIgniter\RESTful\ResourceController;
+ 
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+
 class ApiProduct extends ResourceController
 {
     protected $format = 'json';
     protected $modelName = 'App\Models\Product_model';
+    protected $token = null;
+
+ 	
+
+    public function __construct()
+    {
+        // inisialisasi class Auth dengan $this->protect
+        $this->protect = new Auth();
+    }
+
 
     public function index()
     {
-        return $this->respond($this->model->getProduct(), 200);
+        $secret_key = $this->protect->privateKey();
+        $authHeader = $this->request->getServer('HTTP_AUTHORIZATION');
+        $arr = explode(" ", $authHeader);
+        $token = $arr[1];
+
+            if($token){
+    
+                try {
+            
+                    $decoded = JWT::decode($token, $secret_key, array('HS256'));
+            
+                    // Access is granted. Add code of the operation here 
+                    if($decoded){
+                        // response true
+                        $output = [
+                            'message' => 'Access granted',
+                            'data' => $this->model->getProduct()
+                        ];
+                        return $this->respond($output, 200);
+                    }
+                    
+            
+                } catch (\Exception $e){
+    
+                    $output = [
+                        'message' => 'Access denied',
+                        "error" => $e->getMessage()
+                    ];
+            
+                    return $this->respond($output, 401);
+                }
+            }
     }
+ 	
+        
+
 
     public function create()
     {
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,52 +1,67 @@
 <?php namespace App\Controllers;
-//Panggil JWT
+ 
+// Panggil JWT
 use \Firebase\JWT\JWT;
-//Panggil Controller Auth
+// panggil class Auht
 use App\Controllers\Auth;
+// panggil restful api codeigniter 4
 use CodeIgniter\RESTful\ResourceController;
-
-header("Access-Control-Allow-Origin: *");
-header('Content-type: application/json; charset=UTF-8');
+ 
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding
-,Access-Control-Allow-Headers,Authorization,X-Requested-With");
-
-
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
 class Home extends ResourceController
 {
-	public function __construct()
-	{
-		$this->protect = new Auth();
-	}
-	public function index()
-	{
-		$secret_key = $this->protect->privateKey();
-		$token = null;
-		$authHeader = $this->request->getServer('HTTP_AUTHORIZATION');
-
-		$arr = explode(" ", $authHeader);
-		$token = $arr[1];
-		if ($token) {
-			try {
-				$decoded = JWT::decode($token, $secret_key, array('RS256'));
-				if ($decoded) {
-					$output = [
-						'message' => 'Access granted'
-					];
-					return $this->respond($output, 200);
-				}
-			} catch (\Throwable $th) {
-				$output = [
-					'error' => $th->getMessage()
-				];
-				return $this->respond($output, 401);
-			}
-		} 
+ 
+    public function __construct()
+    {
+        // inisialisasi class Auth dengan $this->protect
+        $this->protect = new Auth();
+    }
+ 
+    public function index()
+    {
+        // ambil dari controller auth function public private key
+        $secret_key = $this->protect->privateKey();
+ 
+        $token = null;
 		
-		// return view('welcome_message');
-	}
-
-	//--------------------------------------------------------------------
-
+        $authHeader = $this->request->getServer('HTTP_AUTHORIZATION');
+	
+        $arr = explode(" ", $authHeader);
+ 
+        $token = $arr[1];
+ 	
+		
+        if($token){
+ 
+            try {
+         
+                $decoded = JWT::decode($token, $secret_key, array('HS256'));
+         
+                // Access is granted. Add code of the operation here 
+                if($decoded){
+                    // response true
+                    $output = [
+                        'message' => 'Access granted'
+                    ];
+                    return $this->respond($output, 200);
+                }
+                 
+         
+            } catch (\Exception $e){
+ 
+                $output = [
+                    'message' => 'Access denied',
+                    "error" => $e->getMessage()
+                ];
+         
+                return $this->respond($output, 401);
+            }
+        }
+    }
+ 
 }
